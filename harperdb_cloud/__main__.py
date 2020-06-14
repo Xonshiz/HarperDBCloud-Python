418abefd238e99c0840e4e8326e68651dbb21b06
Xonshiz <kanojia24.10@gmail.com>
2020-06-14 15:32:28 +0530

    Code Cleanup
    Code Cleanup

--- harperdb_cloud/__main__.py
+++ harperdb_cloud/__main__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+@Author: Xonshiz
+Github: https://github.com/Xonshiz/HarperDBCloud-Python
+"""
 
 import sys
 
@@ -7,17 +11,4 @@ import harperdb_cloud
 
 if __name__ == "__main__":
     harper = harperdb_cloud.HarperDbCloud(sys.argv[1:], instance_url="https://sdktest-xonshiz.harperdbcloud.com/", username="test_user", password="GuessMe24!")
-    # result = harper.create_dev_schema(instance_name="newOne")
-    # result = harper.create_table(schema_name="newOne", table_name="doggo")
-    # result = harper.insert_record(schema_name="newOne", table_name="doggo", records=[{
-    #     "name": "Mark Senior Senior",
-    #     "work": "BORK BORK!"
-    # }])
-    # result = harper.insert_via_csv(schema_name="newOne", table_name="doggo", file_path='/Users/xonshiz/Documents/Book1.csv')
-    # result = harper.run_sql_command(sql_command="UPDATE newOne.doggo SET name = 'Mark Jr.' WHERE name = 'Mark'")
-    # result = harper.update_record(schema_name="newOne", table_name="doggo", records=[{
-    #     "id": "1f6c25f7-793b-4436-bd0c-5aa86ee86d22",
-    #     "name": "Mark Senior Senior",
-    #     "work": "BORK BORK BURK BURK BURK!"
-    # }])
-    # print(result)
+

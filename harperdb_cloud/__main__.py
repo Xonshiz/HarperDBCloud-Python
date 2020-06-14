1faf1284968bc1094d573f061413938b4f825189
Xonshiz <kanojia24.10@gmail.com>
2020-06-14 13:32:51 +0530

    FIrst Commit
    Poject Bones!

---
+++ harperdb_cloud/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+import harperdb_cloud
+
+if __name__ == "__main__":
+    harperdb_cloud.HarperDbCloud(sys.argv[1:])
---
+++ harperdb_cloud/harperdb_cloud.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class HarperDbCloud(object):
+    def __init__(self, argv):
+        print("Running!")
